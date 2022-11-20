f0368164dab0b45f6251cb746d775da67fa893b6
Cicada <202586563@qq.com>
2022-11-20 23:15:45 +0800

    init Project

---
+++ main.js
@@ -0,0 +1,24 @@
+import App from './App'
+import uView from "uview-ui"
+
+// #ifndef VUE3
+import Vue from 'vue'
+
+Vue.config.productionTip = false
+Vue.use(uView)
+App.mpType = 'app'
+const app = new Vue({
+    ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="container">
+		首页
+		<u-button>click</u-button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
